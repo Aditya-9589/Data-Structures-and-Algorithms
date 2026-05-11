a3fce280958ea1516352d8f531a03aede3bf7bb0
Aditya Hariharno <work.adityah@gmail.com>
2026-05-11 22:52:00 +0530

    question 1 completed

--- example_1.java
+++ example_1.java
@@ -1,5 +1,20 @@
-public class example_1 {
-    public static void main(String[] args) {
 
+import java.util.*;
+import java.lang.*;
+
+
+// Q -> Print hello five times ?
+
+public class example_1 {
+
+    static void printHello(int n) {
+        if (n == 0) return;
+
+        System.out.println("Helllo");
+        printHello(n - 1);
+    }
+
+    public static void main(String[] args) {
+        printHello(5);
     }
 }
